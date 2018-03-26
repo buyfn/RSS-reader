3141ce04bcb34661b10277ad14e670556fdddaec
ignat <buyfng@gmail.com>
2018-03-26 14:27:07 +0300

    add close button to modal

--- src/render.js
+++ src/render.js
@@ -14,6 +14,9 @@ export const renderItem = (item) => {
                        <div class="modal-content"/>
                          <div class="modal-header"/>
                            <h3 class="modal-title">${title}</h3>
+                           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                           </button>
                          </div>
                          <div class="modal-body"/>
                            <p>${description}</p>
